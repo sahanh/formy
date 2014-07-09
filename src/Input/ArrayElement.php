b60d9283fbc31076606b59287bd04a9a25c5dfb3
Sahan H <sahan@sahanz.com>
2014-07-09 20:08:20 +0530

    basic array element logic

--- src/Input/ArrayElement.php
+++ src/Input/ArrayElement.php
@@ -1,10 +1,46 @@
 <?php
 namespace SH\Formy\Input;
 
+use SH\Formy\Exception\InvalidValueException;
+
 class ArrayElement extends Element
 {
-	public function setValue($values)
-	{
-		//depends on values create more elements
-	}
+    protected $elements = [];
+
+    public function setValue($values)
+    {
+        if (!is_array($values)) {
+            $given = gettype($values);
+            throw new InvalidValueException("The value should be an array of values {$given}");
+        }
+
+        foreach ($values as $value) {
+            $this->elements[] = $this->makeElement($value);
+        }
+    }
+
+    /**
+     * Get elements
+     * @return array
+     */
+    public function getElements()
+    {
+        if (empty($this->elements))
+            $this->makeElement($value);
+
+        return $this->elements;
+    }
+
+    /**
+     * Make child element with base element spec
+     * @param  mixed $value value for the element
+     * @return Element
+     */
+    public function makeElement($value)
+    {
+        $element = new Element($this->name, $this->type, $this->attributes);
+        $element->setMeta($this->meta);
+
+        return $element;
+    }
 }
